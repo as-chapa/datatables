56affa681565e1b979ec55fc6b06855ad6b73bed
as-chapa <e.goromgorom@gmail.com>
2020-09-03 07:00:42 +0900

    レイアウト調整、詳細ページ作成、一覧から詳細へのリンク追加

--- sample/urls.py
+++ sample/urls.py
@@ -6,4 +6,5 @@ app_name = 'sample'
 urlpatterns = [
     path('booklist/', views.BookList.as_view(), name='booklist'),
     path('booklist_ajax/', views.BookJsonView.as_view(), name='booklist_ajax'),
+    path('bookdetail/<int:pk>', views.BookDetail.as_view(), name='bookdetail'),
 ]
--- sample/views.py
+++ sample/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
 from .models import Book
@@ -16,10 +16,14 @@ class BookJsonView(BaseDatatableView):
 
     def render_column(self, row, column):
         if column == 'id':
-            return f'<a href="sample/booklist/{row.id}">{row.id}</a>'
+            return f'<a href="/sample/bookdetail/{row.id}">{row.id}</a>'
         else:
             return super(BookJsonView, self).render_column(row, column)
 
     # 検索方法の指定：部分一致
     def get_filter_method(self):
         return super().FILTER_ICONTAINS
+
+class BookDetail(DetailView):
+    model = Book
+    template_name = 'sample/book_detail.html'
